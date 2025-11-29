4820eeecc3f168c819964322ed13bee635f6c90e
larleevee <lanavalentine1889@gmail.com>
2025-11-29 17:04:02 +0000

    git yank

--- print.py
+++ print.py
@@ -1,16 +1,13 @@
 import requests
 import os
 
-url = "http://example.com/image.jpg"
-
-fileName = 0
+url = "http://192.168.43.11:5000/image.jpg"
 
 resp = requests.get(url)
 resp.raise_for_status()
 
 with open("image.jpg", "wb") as f:
     f.write(resp.content)
-
 
 os.system("lp image.jpg")
 
