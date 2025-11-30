f8027041d3a87bd6c6445875beab1df098baf0fc
treeinsect <jjpauler69@gmail.com>
2025-11-30 07:57:03 +0000

    updated scene

--- image_server.py
+++ image_server.py
@@ -6,3 +6,5 @@ app = Flask(__name__)
 @app.get("/<image_name>.jpg")
 def passImageToPrinter(image_name: str):
     return send_file("{image}.jpg".format(image=image_name),"image/jpeg")
+
+app.run(host="0.0.0.0",port="8080")
